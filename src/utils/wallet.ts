1115c6ef55ce323ccfc9148a2514565212f5603c
mohaijiang <mohaijiang110@163.com>
2023-08-21 14:42:21 +0800

    change scroll network

--- src/utils/wallet.ts
+++ src/utils/wallet.ts
@@ -8,10 +8,6 @@ const networks = [{
     id: "0x8274f",
     name: "Scroll Sepolia Testnet",
     rpc: "https://sepolia-rpc.scroll.io"
-},{
-    id: "0x82751",
-    name: "Scroll Alpha Testnet",
-    rpc: "https://alpha-rpc.scroll.io/l2",
 }]
 
 export const activeNetwork = networks[0]
